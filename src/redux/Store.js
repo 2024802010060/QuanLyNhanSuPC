0d95e996fdfe822d31a1bcb6c79a4292d6f8cd43
2024802010060 <150000937+2024802010060@users.noreply.github.com>
2024-04-28 16:47:14 +0700

    Update Store.js

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,5 +1,5 @@
-import { createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 
 import { reducer } from './Reducer'
 
-export const store = createStore(reducer)
+export const store = configureStore(reducer)
